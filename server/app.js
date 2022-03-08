da160ba4e4abe73eff9521392128993cc1eb15b5
pratik-18 <pratikkakadiya18@gmail.com>
2022-03-08 11:09:31 +0100

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Document = require("./Document")
+
+const { Socket } = require("socket.io");
+
+const defaultValue = "";
+mongoose
+    .connect("mongodb://localhost:27017/googledocs", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected Successfully");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const io = require("socket.io")(3001, {
+    cors: {
+        origin: "http://localhost:3000",
+        method: ['GET', 'POST']
+    }
+});
+
+io.on("connection", socket => {
+    socket.on("get-document", async documentId => {
+        const document = await findOrCreate(documentId);
+        socket.join(documentId);
+        socket.emit('load-document', document.data);
+
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        })
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
+    })
+})
+
+async function findOrCreate(id) {
+    if (id == null) return;
+    const document = await Document.findById(id);
+    if (document) return document;
+    return Document.create({ _id: id, data: defaultValue })
+}
